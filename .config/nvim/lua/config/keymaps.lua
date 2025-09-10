fdd31f75948e2ca8b86da699b04b1616c1498226
tkmrrkn <tkmrrkn.0609@gmail.com>
2025-09-11 00:26:10 +0900

    {}, "", ''の補完

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,12 @@ local map = vim.keymap.set
 
 map("n", "<leader>w", ":w<CR>")
 map("n", "<leader>q", ":q<CR>")
+map("n", "<leader>e", ":NvimTreeToggle<CR>")
+
 map("i", "jj", "<Esc>")
 map("i", "jk", "<Esc>")
-map("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+map("i", "{<Enter>", "{}<Left><CR><Esc><S-o>", { noremap = true})
+map("i", "{", "{}<Left>", { noremap = true })
+map("i", "\"", "\"\"<Left>", { noremap = true })
+map("i", "'", "''<Left>", { noremap = true })
