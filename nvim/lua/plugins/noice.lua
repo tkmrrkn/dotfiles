000cf7770249c6474f3b991c36b9ef667005ae7e
Takemura Ryoken <tkmrrkn.0609@gmail.com>
2026-07-12 15:51:32 +0900

    nvim: noice.nvimでコマンドライン/通知をポップアップ表示に
    コマンドラインとLSP進捗通知等をフローティングUIに置き換え、
    コマンドパレットは画面中央に表示するよう調整。

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,35 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      lsp_doc_border = true,
+    },
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "50%",
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+      },
+    },
+  },
+}
