f5b955da6a0d6ba8d601f9093dc3ddfb37d00184
Takemura Ryoken <tkmrrkn.0609@gmail.com>
2026-07-12 23:23:06 +0900

    nvim: Esc代替とウィンドウ操作のキーマップを追加
    - insertモードのjkをEscに割り当て、ホームポジションから手を離さずに戻れるように
    - Ctrl+hjklで分割ウィンドウ間を直接移動できるように
    - leader+sv/shで左右/上下分割を素早く作成できるように

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Escの代替" })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "左のウィンドウへ移動" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "下のウィンドウへ移動" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "上のウィンドウへ移動" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "右のウィンドウへ移動" })
+
+vim.keymap.set("n", "<leader>sv", "<cmd>vsplit<cr>", { desc = "左右に分割" })
+vim.keymap.set("n", "<leader>sh", "<cmd>split<cr>", { desc = "上下に分割" })
+
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(event)
     local map = function(keys, fn, desc)
