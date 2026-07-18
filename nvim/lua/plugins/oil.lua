d25a021abb6d0e0e37cb7cdbfa3ee12a37d18dbb
Takemura Ryoken <tkmrrkn.0609@gmail.com>
2026-07-18 15:37:26 +0900

    nvim: oil.nvimをlazy=falseにして起動時に有効化
    keysのみの遅延ロードだとディレクトリを開いた際にnetrwの代わりに
    起動するフックが登録されず、oilが機能していなかった。

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,7 @@
 return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = false,
   opts = {},
   keys = {
     { "-", "<cmd>Oil<cr>", desc = "親ディレクトリを開く(oil)" },
