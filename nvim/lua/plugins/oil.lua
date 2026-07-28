1e2864865801d2a7800c65b641dac3b3d356e1fe
Takemura Ryoken <tkmrrkn.0609@gmail.com>
2026-07-28 19:33:02 +0900

    nvim: oilで隠しファイルをデフォルト表示する

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   lazy = false,
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
   keys = {
     { "-", "<cmd>Oil<cr>", desc = "親ディレクトリを開く(oil)" },
   },
